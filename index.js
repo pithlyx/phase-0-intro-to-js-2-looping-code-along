4b79e53d65b90694288c1c44def634394af60941
Cody Roberts <pithlyx@gmail.com>
2023-04-20 12:53:43 -0600

    loop practice

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(num){
+    while (num >= 0){
+        console.log(num);
+        --num;
+    }
+}
+
+function writeCards
+(names, type){
+    let cards = [];
+    for (let i = 0; i < names.length; i++){
+        cards[i] = (`Thank you, ${names[i]}, for the wonderful ${type} gift!`);
+    }
+    return cards;
+}
